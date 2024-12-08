5b94a3c933b9416507b8e1ba4333d435b2b5efce
Serban Ghita <serbanghita@gmail.com>
2024-12-08 21:56:10 +0200

    Version bump
    Signed-off-by: Serban Ghita <serbanghita@gmail.com>

--- src/MobileDetect.php
+++ src/MobileDetect.php
@@ -19,7 +19,7 @@
  * @author  Nick Ilyin <nick.ilyin@gmail.com>
  * @author: Victor Stanciu <vic.stanciu@gmail.com> (original author)
  *
- * @version 4.8.06
+ * @version 4.8.07
  */
 
 declare(strict_types=1);
@@ -241,7 +241,7 @@ class MobileDetect
     /**
      * Stores the version number of the current release.
      */
-    protected string $VERSION = '4.8.06';
+    protected string $VERSION = '4.8.07';
 
     protected array $config = [
         // Auto-initialization on HTTP headers from $_SERVER['HTTP...']
